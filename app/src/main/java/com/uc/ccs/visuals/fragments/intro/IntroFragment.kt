1cc78de36dd81ec7f197e4f42efd37dc15d5307f
Elbert <elbertluta@gmail.com>
2023-05-04 20:50:54 +0800

    added intro screen

--- app/src/main/java/com/uc/ccs/visuals/fragments/intro/IntroFragment.kt
+++ app/src/main/java/com/uc/ccs/visuals/fragments/intro/IntroFragment.kt
@@ -1,29 +1,28 @@
-package com.uc.ccs.visuals.fragments.splashscreen
+package com.uc.ccs.visuals.fragments.intro
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
 import com.uc.ccs.visuals.R
+import com.uc.ccs.visuals.databinding.FragmentIntroBinding
 import com.uc.ccs.visuals.databinding.FragmentSplashScreenBinding
 
-class SplashFragment : Fragment() {
+class IntroFragment : Fragment() {
 
-    private var _binding: FragmentSplashScreenBinding? = null
+    private var _binding: FragmentIntroBinding? = null
     private val binding get() = _binding!!
-
-    private val SPLASH_TIME_OUT = 3000L // 3 seconds delay
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+
+        _binding = FragmentIntroBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -31,10 +30,14 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-                  findNavController().navigate(R.id.action_splashScreenFragment_to_introFragment)
-        }, SPLASH_TIME_OUT)
-
+        binding.setupViews()
     }
+
+    private fun FragmentIntroBinding.setupViews() {
+        tvLetsGo.setOnClickListener {
+            findNavController().navigate(R.id.action_introFragment_to_mapFragment)
+        }
+    }
+
 
 }
