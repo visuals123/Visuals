cf22656544fb16dd70f6d315d83cf840b1cbbc65
Julius Wilmar <wilmarjulius@gmail.com>
2023-06-17 18:06:58 +0800

    Initial Merging

--- app/src/main/java/com/uc/ccs/visuals/screens/main/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/uc/ccs/visuals/screens/main/adapter/ViewPagerAdapter.kt
@@ -23,10 +23,11 @@ class ViewPagerAdapter(private val context: Context, private val items: List<Mar
         _binding = ViewPagerItemBinding.inflate(inflater, container, false)
 
         val item = items[position]
+        val imageResource = getRoadSignImageResource(item.code)
 
         with(binding) {
             Glide.with(context)
-                .load(item.iconImageUrl)
+                .load(imageResource)
                 .into(iconImageView)
             tvDistance.text = formatDistance(item.distance ?: 0.0)
             tvTitle.text = item.title
@@ -58,5 +59,24 @@ class ViewPagerAdapter(private val context: Context, private val items: List<Mar
 
     override fun isViewFromObject(view: View, obj: Any): Boolean {
         return view == obj
+    }
+
+    enum class RoadSignCode(val code: String, val imageResource: Int) {
+        R1_1("R1-1", R.drawable.sign_stop_sign),
+        R1_2("R1-2", R.drawable.sign_yield_sign),
+        R2_1("R2-1", R.drawable.sign_one_way_sign),
+        R3_1("R3-1", R.drawable.sign_no_entry_sign),
+        R3_15("R3-15", R.drawable.sign_no_u_turn_sign),
+        R3_14("R3-14", R.drawable.sign_no_left_turn_sign),
+        R3_13("R3-13", R.drawable.sign_no_right_turn_sign),
+        R3_16("R3-16", R.drawable.sign_no_overtake_sign),
+        R4_1("R4-1", R.drawable.sign_speed_limit_sign),
+        R4_3("R4-3", R.drawable.sign_minimum_speed_limit_sign),
+        R5_1S("R5-1S", R.drawable.sign_no_parking_sign)
+    }
+
+    fun getRoadSignImageResource(code: String): Int {
+        val roadSign = RoadSignCode.values().find { it.code == code }
+        return roadSign?.imageResource ?: R.drawable.sign_loading
     }
 }
